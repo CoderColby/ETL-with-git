cb1e77d77cec1607cf1f3435f4ae5cfce5afbe3e
Colby Anderson <colbyteruo@gmail.com>
2023-10-08 15:17:23 -0400

    Still figuring out how this VS code git stuff works

---
+++ Code/Main.java
@@ -0,0 +1,10 @@
+// import Code.GameWindow;
+
+class Main {
+
+  public static final GameWindow mainWindow = new GameWindow();
+  
+  public static void main(String[] args) {
+    // new GameWindow();
+  }
+}
